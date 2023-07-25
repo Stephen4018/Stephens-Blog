3b9f07f8568f4cb61bb96cf7ad6d7612d3bf1b22
stephen4018 <stephenohaeri4018@gmail.com>
2023-07-25 12:51:30 +0100

    first commit

---
+++ Data/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    internal class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
