d8cbb6f67b83097506c1bd6fd4034db29523732f
stephen4018 <stephenohaeri4018@gmail.com>
2023-07-27 01:16:49 +0100

    added auto mapper

---
+++ Blogging/Mapper/BlogProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Blogging.DTO;
+using Data.Models;
+
+namespace Blogging.Mapper
+{
+    public class BlogProfile : Profile
+    {
+        public BlogProfile()
+        {
+            CreateMap<CreateBlogDto, Blogs>();
+
+        }
+    }
+}
